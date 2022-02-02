2ac33a5902c2f4ba2e58b3912288460c690f3f87
MainSilent <70273681+MainSilent@users.noreply.github.com>
2022-02-02 12:58:24 +0330

    add and setup react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom"
 import Navbar from './Navbar'
 import Home from './Home'
 import Footer from './Footer'
@@ -6,7 +11,20 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+
+      <Router>
+        <Switch>
+          <Route path={process.env.PUBLIC_URL+'/'} exact>
+            <Home/>
+          </Route>
+          <Route path='*'>
+            <h1 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw', fontSize: 26 }}>
+              404 - Page Not Found
+            </h1>
+          </Route>
+        </Switch>
+      </Router>
+
       <Footer />
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Logo from './assets/images/logo.png'
 
 function Items() {
